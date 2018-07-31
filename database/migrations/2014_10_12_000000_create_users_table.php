00c40b370948811052b4ca3f5c4ddf3fe5c6c7a0
Marko Stojanović <marcofolk@yahoo.com>
2018-07-31 02:45:06 +0200

    migrations&blades

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,21 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('user_id');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('gender', ['female', 'male','other'])->nullable();;
+            $table->string('city')->nullable();;
+            $table->string('state')->nullable();;
+            $table->date('birthday')->nullable();;
+
+
             $table->rememberToken();
             $table->timestamps();
+
+            $table->engine = 'InnoDB';
         });
     }
 
